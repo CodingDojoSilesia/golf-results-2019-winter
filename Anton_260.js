f180c42fc5fc0a62409c1237158ea38b5c76c39b
Jacek <namiota@gmail.com>
2020-01-12 14:41:27 +0100

    Antons solution

---
+++ Anton_260.js
@@ -0,0 +1,1 @@
+q=a="",z=x=>{for(i of"vv>^^>")a+=x&1?"*"+i:i,x>>=1},y=(e,r=0)=>e.charCodeAt(r)-32;for(p of process.argv[2])u=y(p),q=u?q:0,q!=u<26&u>14?z((q=!q)?60:24):0,z(y(`)I9+YLJON"$ R`,"ąćęłńóśżź,. 0".indexOf(p))|y("!#AQ1CS3BR%'EU5GW7FV-/ZM]=",u-(q?17:65)));console.log(a)
